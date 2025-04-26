89cbe856de2c0f74a37054fc10a01bc9faa353c1
lando <orlandroide@gmail.com>
2025-04-26 14:19:03 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lando Ando on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
